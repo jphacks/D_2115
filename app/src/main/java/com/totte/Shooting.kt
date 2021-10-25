2e58a10a1429374497c88cfa2e62c23f4ca7792e
manabinohibi <nishimura@db.is.i.nagoya-u.ac.jp>
2021-10-25 13:55:59 +0900

    カメラのパーミッションを取得できるように

--- app/src/main/java/com/totte/Shooting.kt
+++ app/src/main/java/com/totte/Shooting.kt
@@ -1,13 +1,37 @@
 package com.totte
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class Shooting : AppCompatActivity() {
+    companion object {
+        const val CAMERA_REQUEST_CODE = 1
+        const val CAMERA_PERMISSION_REQUEST_CODE = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shooting)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        if (checkCameraPermission()) {
+            shootPicture()
+        } else {
+            grantCameraPermission()
+        }
+    }
+
+    private fun checkCameraPermission() = PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+
+    private fun grantCameraPermission() = ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+    private fun shootPicture() {
 
     }
 }
