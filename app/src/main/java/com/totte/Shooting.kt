708654024cf3e55727bc2197708d13c594ad8cbc
manabinohibi <nishimura@db.is.i.nagoya-u.ac.jp>
2021-10-26 13:24:34 +0900

    一時ファイル作成

--- app/src/main/java/com/totte/Shooting.kt
+++ app/src/main/java/com/totte/Shooting.kt
@@ -1,27 +1,28 @@
 package com.totte
 
 import android.Manifest
-import android.R.attr
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import android.R.attr.data
-
-import android.R.attr.bitmap
-
-
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 
 class Shooting : AppCompatActivity() {
-
     companion object {
         const val CAMERA_REQUEST_CODE = 1
         const val CAMERA_PERMISSION_REQUEST_CODE = 2
+        var currentPhotoPath: String? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,11 +66,23 @@ class Shooting : AppCompatActivity() {
         startActivityForResult(intent, CAMERA_REQUEST_CODE)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun savePicture(data: Intent?) {
+        val file = File.createTempFile("tmp", null, cacheDir)
+        
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         // ここで撮影したデータを送信処理？
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            this.savePicture(data = data)
+        }
 
         finish()
     }
+
 }
