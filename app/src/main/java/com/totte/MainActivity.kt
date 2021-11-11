cdb87e670e9d7a72ca611e6bd4d07cc4f5468fdf
manabinohibi <nishimura@db.is.i.nagoya-u.ac.jp>
2021-11-11 13:47:21 +0900

    匿名認証を有効化

--- app/src/main/java/com/totte/MainActivity.kt
+++ app/src/main/java/com/totte/MainActivity.kt
@@ -3,13 +3,27 @@ package com.totte
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        Log.d("current", "currentUser = ${auth.currentUser?.uid.toString()}")
+        signInAnonymously()
 
         val btnSearchBegin : Button = findViewById(R.id.btnSearchBegin)
         val name : EditText = findViewById(R.id.name)
@@ -19,5 +33,16 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("NAME",name.text.toString())
             startActivity(intent)
         }
+    }
+
+    private fun signInAnonymously() {
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("success", "currentUser = ${auth.currentUser?.uid.toString()}")
+                } else {
+                    Log.d("error", task.exception.toString())
+                }
+            }
     }
 }
