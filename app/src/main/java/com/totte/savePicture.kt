e216e86bd18e3f06258acdfbde3650a9c8249789
manabinohibi <nishimura@db.is.i.nagoya-u.ac.jp>
2021-11-16 15:59:57 +0900

    create new activity

---
+++ app/src/main/java/com/totte/savePicture.kt
@@ -0,0 +1,11 @@
+package com.totte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class savePicture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_picture)
+    }
+}
