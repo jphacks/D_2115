5e4bdbd0d53389318dfac548e4c678b0a67c0a04
shu-nkym <nakayama@db.is.i.nagoya-u.ac.jp>
2021-11-16 16:47:27 +0900

    searching ...の実装

--- app/src/main/java/com/totte/MainActivity.kt
+++ app/src/main/java/com/totte/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -16,6 +18,8 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+
+    //private var shortAnimationDuration: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +32,17 @@ class MainActivity : AppCompatActivity() {
         val btnSearchBegin : Button = findViewById(R.id.btnSearchBegin)
         val btnChatBegin : Button = findViewById(R.id.btnChatBegin)
         val name : EditText = findViewById(R.id.name)
+        val searchingText : TextView = findViewById(R.id.searchingText)
+
+        searchingText.visibility = View.GONE
+        //shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+
 
         btnSearchBegin.setOnClickListener {
+
+            searchingText.visibility = View.VISIBLE
+            btnSearchBegin.visibility = View.GONE
+
             val intent = Intent(this, MainActivity2::class.java)
             intent.putExtra("NAME",name.text.toString())
             startActivity(intent)
@@ -40,6 +53,15 @@ class MainActivity : AppCompatActivity() {
             // intent.putExtra("UID", auth.currentUser?.uid.toString())
             startActivity(intent)
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        val searchingText : TextView = findViewById(R.id.searchingText)
+        val btnSearchBegin : Button = findViewById(R.id.btnSearchBegin)
+        searchingText.visibility = View.GONE
+        btnSearchBegin.visibility = View.VISIBLE
     }
 
     private fun signInAnonymously() {
